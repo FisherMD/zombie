2d3f6a4c51ef6044c898c8fd6eb52a6a5b8d8f29
Waffle <Waffle@10.253.172.69>
2017-02-22 17:59:36 -0600

    Added two more zombies (one leg and demon zombies).

---
+++ src/zombie/DemonZombie.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package zombie;
+
+import intf.*;
+
+
+/**
+ *
+ * @author Waffle
+ */
+public class DemonZombie extends Character implements IZombie
+{
+   public DemonZombie(){
+      super(120);
+   }
+   
+   public void attack(ISurvivor survivor){
+      survivor.decreaseHealth(30);
+   }
+   public String getIZombie(){
+      return "DemonZombie";
+   }
+   
+}
